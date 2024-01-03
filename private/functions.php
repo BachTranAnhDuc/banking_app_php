4522ce382f70f23762b9da35dc6a9d74e0538f29
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-03 16:34:54 +0700

    feat(setup): endcode url and html

--- private/functions.php
+++ private/functions.php
@@ -1,5 +1,18 @@
 <?php
 function url_for($script_path) {
     return WWW_ROOT . $script_path;
-  }
+}
+
+function ued($string = "") {
+    return urlencode($string);
+}
+
+function raw_ued($string = "") {
+    return rawurlencode($string);
+}
+
+function hsc($string = "") {
+    return htmlspecialchars($string);
+}
+
 ?>
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,8 +4,20 @@
 
 <?php include(SHARE_PATH . '/staff/header/header.php') ?>
 
+<?php
+$id = $_GET['id'] ?? '1';
+?>
+
 <div class="max-w-7xl w-full bg-blue-50 px-8 py-4 justify-self-center">
-    <h1><?php echo $_GET['id'] ?></h1>
+    <h1><?php echo hsc($id) ?></h1>
+
+    <ul>
+        <li><a href="show.php?name=<?php echo ued("Anh Duc")?>">Link 1</a></li>
+
+        <li><a href="show.php?company=<?php echo ued("Anh Duc")?>">Link 2</a></li>
+
+        <li><a href="show.php?query=<?php echo ued("*!@#")?>">Link 3</a></li>
+    </ul>
 </div>
 
 
