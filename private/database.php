8bd758bb5f20b5d24f01f745d3a90c5079e70685
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-04 14:53:24 +0700

    feat(sql): handle error

--- private/database.php
+++ private/database.php
@@ -5,13 +5,7 @@ require_once('db_credentials.php');
 function db_connect() {
     $connection =  mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
-    // if (!$connection) {
-    //     die("Connection failed: " . mysqli_connect_error());
-    // }
-    // else {
-    //     echo "Connect successfully";
-    // }
-
+    confirm_db_connect();
 
     return $connection;
 }
@@ -19,6 +13,21 @@ function db_connect() {
 function db_disconnect($connection) {
     if (isset($connection)) {
         mysqli_close($connection);
+    }
+}
+
+function confirm_db_connect() {
+    if (mysqli_connect_errno()) {
+        $msg = "Database connection failed: ";
+        $msg .= mysqli_connect_error();
+        $msg .= " (" . mysqli_connect_errno() . ")";
+        exit($msg);
+    }
+}
+
+function confirm_result_set($result_set) {
+    if (!$result_set) {
+        exit("Database query failed!.");
     }
 }
 
